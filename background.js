216c7501c10042ca9d21f06b6d3e4286f8aa03fe
toaaa <toa@toaaa.de>
2023-05-25 01:10:36 +0200

    Amogim

---
+++ background.js
@@ -0,0 +1,10 @@
+﻿chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status == "complete") {
+      const url = new URL(tab.url);
+      if (url.hostname == "portal.suchtbunker.de" && url.searchParams.has("s")) {
+        const steamId = url.searchParams.get("s");
+        const externalUrl = "https://util.suchtbunker.de/gmod_load/darkrp/index.php?s=" + steamId;
+        chrome.tabs.sendMessage(tabId, { action: "fetchHtml", url: externalUrl });
+      }
+    }
+  });
---
+++ content.js
@@ -0,0 +1,51 @@
+﻿chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action == "fetchHtml") {
+        fetchHtml(request.url);
+    }
+});
+
+function fetchHtml(url) {
+    fetch("https://origins.galaxycrow.de/raw?url=" + url)
+        .then(response => response.text())
+        .then(html => extractTextFromHtml(html))
+        .then(text => insertHtml(text))
+        .catch(error => console.error(error));
+}
+
+function extractTextFromHtml(html) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    const paragraphs = doc.querySelectorAll("p");
+    for (const p of paragraphs) {
+        if (p.textContent.includes("RP:")) {
+            return p.textContent.replace("RP:", "RP-Punkte:");
+        }
+    }
+    return null;
+}
+
+function insertHtml(text) {
+  setTimeout(() => {
+    const h5 = document.createElement("h5");
+    h5.textContent = text;
+    const h5Elements = document.querySelectorAll("h5");
+
+    let gangH5 = null;
+    for (const h5Element of h5Elements) {
+      if (h5Element.textContent.includes("Gang:")) {
+        gangH5 = h5Element;
+        break;
+      }
+      if (h5Element.textContent.includes("Geld (Bank):")) {
+        gangH5 = h5Element;
+      }
+    }
+
+    if (gangH5) {
+      const container = gangH5.parentNode;
+      container.insertBefore(h5, gangH5.nextSibling);
+    } else {
+      console.error("Header not found");
+    }
+  }, 50);
+}
